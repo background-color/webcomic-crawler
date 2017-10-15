2a0b0d6e3ec2c67ce24e8cddf2095259055cc0d4
Ubuntu <ubuntu@ip-172-31-13-67.ap-northeast-1.compute.internal>
2017-10-15 16:53:01 +0900

    微調整

--- app/crawl.php
+++ app/crawl.php
@@ -1,6 +1,5 @@
 <?php
-require_once '/var/www/webcomic_crawler/app/include.php';
-//require_once dirname(__FILE__) . '/include.php';
+require_once dirname(__FILE__) . '/include.php';
 
 require_once APP_PATH . 'class_web_comic_rss.php';
 $objWCR = new WebComicRss;
